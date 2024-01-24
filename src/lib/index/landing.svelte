cccca1244f51e33afadfef371b3589d129f2064f
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-01-23 22:12:57 -0600

    Darken bananas more on mobile.

--- src/lib/index/landing.svelte
+++ src/lib/index/landing.svelte
@@ -39,7 +39,7 @@
         </div>
     </div>
 
-    <div class="w-screen h-screen md:w-[45vw] md:h-[75vh] lg:h-[65vh] absolute right-0 top-1/2 -translate-y-1/2 blur-sm brightness-90 md:blur-none md:brightness-100">
+    <div class="w-screen h-screen md:w-[45vw] md:h-[75vh] lg:h-[65vh] absolute right-0 top-1/2 -translate-y-1/2 blur-sm brightness-75 md:blur-none md:brightness-100">
         {#each Array(4) as _value, quadrant}
             <div class="absolute w-[calc(50%-8%)] h-[calc(50%-8%)]" style="top: calc({25 * (quadrant == 0 || quadrant == 1 ? 0 : 2)}% + {8 * (quadrant == 0 || quadrant == 1 ? -1 : 1)}%); left: calc({25 * (quadrant == 0 || quadrant == 2 ? 0 : 2)}% + {8 * (quadrant == 0 || quadrant == 2 ? -1 : 1)}%);">
                 {#each Array(4) as _value, _index}
