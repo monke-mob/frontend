04b6c33e4005d14aa35a5f849e665de65ad8b039
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-01-23 21:17:05 -0600

    Add contact component.

---
+++ src/lib/index/contact.svelte
@@ -0,0 +1,3 @@
+<div class="w-screen py-[10vh] bg-no-repeat bg-gradient-to-b from-brandSecondary to-brand drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+    <p class="brandTitle">contact us</p>
+</div>
