e7db83d064ed7e16705b79e046f3b8a85b384f08
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-01-23 21:35:49 -0600

    Create brand gradient component.

--- src/lib/index/contact.svelte
+++ src/lib/index/contact.svelte
@@ -1,3 +1,3 @@
-<div class="w-screen py-[10vh] bg-no-repeat bg-gradient-to-b from-brandSecondary to-brand drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+<div class="w-screen py-[10vh] brandGradient">
     <p class="brandTitle">contact us</p>
 </div>
