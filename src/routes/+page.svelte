5d13e67a9d043a4cd314a2ff5868fe5978228113
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-01-23 21:17:17 -0600

    Implement contact component.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
     import Landing from "$lib/index/landing.svelte";
     import Projects from "$lib/index/projects.svelte";
+    import Contact from "$lib/index/contact.svelte";
 </script>
 
 <Landing />
 <Projects />
+<Contact />
