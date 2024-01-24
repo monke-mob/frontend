37bfdb78cf5ea850b854edb0cdc23c5be03cebe9
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-01-23 20:59:55 -0600

    Improve landing component.

--- src/lib/index/landing.svelte
+++ src/lib/index/landing.svelte
@@ -1,8 +1,90 @@
-<div class="w-screen h-screen flex flex-col gap-4 justify-center items-center relative">
-    <img class="h-36" src="/images/logoText.svg" alt="monke mob" />
-    <p class="text-2xl text-light">Closed Alpha Release<br>March 10th 2024</p>
+<script lang="ts">
+    // This determines if the landing will display an announcement or branding.
+    const isAnnouncement: boolean = false;
+</script>
 
-    <div class="absolute flex bottom-12 left-1/2 -translate-x-1/2">
-        <a class="button" href="/">Learn more</a>
-    </div>
+<div class="w-screen h-screen flex flex-col gap-4 justify-center items-center relative">
+    {#if isAnnouncement}
+        <img class="h-36" src="/images/logoText.svg" alt="monke mob" />
+
+        <!--Change this text for announcements.-->
+        <p class="text-2xl text-light">Closed Alpha Release<br />March 10th 2024</p>
+
+        <div class="flex mt-6">
+            <a class="button" href="/">Learn more</a>
+        </div>
+    {:else}
+        <div class="w-[45vw] h-[65vh] absolute right-0 top-1/2 -translate-y-1/2">
+            {#each Array(4) as _value, quadrant}
+                <div class="absolute w-1/2 h-1/2" style="top: calc({25 * (quadrant == 0 || quadrant == 1 ? 0 : 2)}% - 20px); left: calc({25 * (quadrant == 0 || quadrant == 2 ? 0 : 2)}% - 20px);">
+                    {#each Array(4) as _value, _index}
+                        <svg class="banana aspect-square absolute fill" style="--rotation: {Math.random() * (Math.random() < 0.5 ? -50 : 50)}deg; --delay: {Math.random() * 3}s; width: {Math.random() * 20 + 30}px; top: {Math.random() * 100}%; left: {Math.random() * 100}%;" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 128 128" xml:space="preserve">
+                            <defs>
+                                <linearGradient id="0" x1="0.5" y1="0" x2="0.5" y2="1">
+                                    <stop offset="0%" stop-color="#ffd160" />
+                                    <stop offset="33.33%" stop-color="#ffc549" />
+                                    <stop offset="100%" stop-color="#ffad00" />
+                                </linearGradient>
+                            </defs>
+
+                            <path
+                                fill="url(#0)"
+                                d="M115.7,103.1c3.9,4.6,2,10.6,0.3,13.4c-0.1,0.2-0.3,0.4-0.5,0.6c-7.4,7.1-18.9,10.9-32.2,10.9
+	c-1.8,0-3.6-0.1-5.4-0.2c-20.9-1.6-40.3-12-53.2-28.5c-1.4-1.8-5.7-7.2-9.2-14.9c-4.5-9.7-6.2-19.4-5.2-29
+	c0.5-5.2,2.6-18.6,11.6-29.2c2.4-2.8,4.5-4.6,6.6-6.4c2.6-2.2,5.3-4.5,8.8-9c2-2.5,3.7-5.2,5.5-8.3c0.9-1.6,2.6-2.6,4.4-2.6
+	s3.5,1.1,4.3,2.8c1.2,2.5,2.5,4.9,3.8,7.4c0,0,0,0,0,0.1c0.7,1.3,0.8,2.8,0.3,4.2c-0.5,1.4-1.5,2.5-2.7,3.1
+	c-1.5,0.7-3.7,1.9-5.6,4.1c0,0-2.6,3-3.8,7.7c-2,7.8-2.7,25.1,7.9,39.9c7.7,10.8,18,15.1,23.4,16.7c3.9,1.1,7.3,2.1,10.4,3
+	C104.4,94.4,109.6,95.8,115.7,103.1L115.7,103.1z"
+                            />
+                        </svg>
+                    {/each}
+                </div>
+            {/each}
+
+            <div class="w-[25vw] aspect-square absolute right-40 top-1/2 -translate-y-1/2 z-[2]">
+                <img
+                    class="banner w-full h-full"
+                    alt="monke mob banner"
+                    src="/images/monke-mob/banner/400x400.png"
+                    srcset="/images/monke-mob/banner/400x400.png 320w, 
+            /images/monke-mob/banner/600x600.png 640w, 
+            /images/monke-mob/banner/900x900.png 960w"
+                    sizes="(max-width: 320px) 280px, 
+                   (max-width: 640px) 640px, 
+                   960px"
+                />
+            </div>
+        </div>
+
+        <div class="absolute left-40">
+            <p class="text-2xl text-light">going bananas 🍌</p>
+        </div>
+    {/if}
 </div>
+
+{#if !isAnnouncement}
+    <style lang="postcss">
+        .banner {
+            --rotation: 0deg;
+        }
+        
+        .banner, .banana {
+            animation: hover infinite 6s ease-in-out var(--delay);
+            transform: translateY(0px) rotate(var(--rotation));
+        }
+
+        @keyframes hover {
+            0% {
+                transform: translateY(0px) rotate(var(--rotation));
+            }
+
+            50% {
+                transform: translateY(-15px) rotate(var(--rotation));
+            }
+
+            100% {
+                transform: translateY(0px) rotate(var(--rotation));
+            }
+        }
+    </style>
+{/if}
