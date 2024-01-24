76fcacad37ad49114a91599cf63b2436d5cfc8d9
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-01-23 21:17:41 -0600

    Increase padding.

--- src/lib/index/components/project.svelte
+++ src/lib/index/components/project.svelte
@@ -6,7 +6,7 @@
     export let buttons: { text: string; link: string; image: string; style: string }[];
 </script>
 
-<div class="h-fit w-screen flex justify-center items-center gap-10 lg:gap-16 p-8 px-6 md:px-20 lg:px-32 py-[12vh] lg:py-[5vh] {format === 'right' ? 'flex-col-reverse lg:flex-row-reverse' : 'flex-col-reverse lg:flex-row'}">
+<div class="h-fit w-screen flex justify-center items-center gap-10 lg:gap-16 p-8 px-6 md:px-20 lg:px-32 py-[12vh] lg:py-[20vh] {format === 'right' ? 'flex-col-reverse lg:flex-row-reverse' : 'flex-col-reverse lg:flex-row'}">
     <div class="h-fit text-center lg:text-left lg:h-full w-full lg:w-[50%] py-2 flex justify-center items-center flex-col lg:items-start {format === "right" ? "" : ""}">
         <p class="text-3xl mb-2 font-bold">{name}</p>
         <p class="text-xl">{description}</p>
