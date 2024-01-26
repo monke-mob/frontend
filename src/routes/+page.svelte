4309d142169d23f2945275f0d0cdc56ce37e8957
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-01-25 20:43:40 -0600

    Implement new components.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
     import Landing from "$lib/index/landing.svelte";
+    import LatestNews from "$lib/index/latestNews.svelte";
+    import AboutUs from "$lib/index/aboutUs.svelte";
     import Projects from "$lib/index/projects.svelte";
     import Contact from "$lib/index/contact.svelte";
 </script>
 
 <Landing />
+<LatestNews />
+<AboutUs />
 <Projects />
 <Contact />
