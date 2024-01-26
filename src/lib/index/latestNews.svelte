75587821eea10ff431b71843e009515272eb8eea
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-01-25 20:43:12 -0600

    Add latest news component.

---
+++ src/lib/index/latestNews.svelte
@@ -0,0 +1,15 @@
+<div class="w-screen h-[70vh] py-[20vh] flex justify-center items-center gap-20">
+    <div class="w-1/2 h-full flex justify-end items-center">
+        <div class="h-full aspect-square bg-red-500 rounded-2xl"></div>
+    </div>
+
+    <div class="w-1/2 h-full flex flex-col justify-center items-start gap-5 text-left">
+        <p class="font-ZCOOLKuaiLe font-extrabold brandGradient text-lg" style="-webkit-background-clip: text; -webkit-text-fill-color: transparent;">latest news</p>
+
+        <div>
+            <p class="text-3xl gap-2 brandTitle">Closed Alpha Release</p>
+        </div>
+
+        <a class="button">Read more</a>
+    </div>
+</div>
