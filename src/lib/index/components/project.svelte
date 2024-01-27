a43146e2ff405db10b1ea346f448da36ed6d7ba2
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-01-27 13:34:59 -0600

    Improve text size on mobile.

--- src/lib/index/components/project.svelte
+++ src/lib/index/components/project.svelte
@@ -10,7 +10,7 @@
 <div class="h-fit w-screen flex justify-center items-center gap-10 lg:gap-16 p-8 px-6 md:px-20 lg:px-32 py-[12vh] lg:py-[20vh] {format === 'right' ? 'flex-col-reverse lg:flex-row-reverse' : 'flex-col-reverse lg:flex-row'}">
     <div class="h-fit text-center lg:text-left lg:h-full w-full lg:w-[50%] py-2 flex justify-center items-center flex-col lg:items-start {format === "right" ? "" : ""}">
         <p class="text-3xl mb-2 font-bold">{name}</p>
-        <p class="text-xl">{description}</p>
+        <p class="text-lg sm:text-xl">{description}</p>
 
         <div class="flex gap-2 mt-8 flex-wrap justify-center lg:justify-start">
             {#each buttons as button}
