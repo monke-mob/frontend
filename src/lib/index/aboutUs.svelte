c4bf8dfea6514cc13659fe9fd7f5a1c57ff9ff48
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-01-27 13:47:20 -0600

    Improve text size on smaller screens.

--- src/lib/index/aboutUs.svelte
+++ src/lib/index/aboutUs.svelte
@@ -3,11 +3,5 @@
 <div class="w-screen h-fit py-[15vh] px-6 sm:px-14 lg:px-32" id="#aboutUs">
     <p class="brandTitle">about<br>us</p>
 
-    <p class="pt-10 text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora nulla odio ipsum sit asperiores alias iste nesciunt architecto iusto quam, excepturi illo dolor eligendi accusamus, explicabo maxime voluptatem tempore velit. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia nisi doloremque molestiae temporibus illo eos quod sunt vel quo aliquid labore necessitatibus iure ratione, molestias voluptas doloribus minima dicta consequuntur?</p>
-
-    <p class="pt-10 text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora nulla odio ipsum sit asperiores alias iste nesciunt architecto iusto quam, excepturi illo dolor eligendi accusamus, explicabo maxime voluptatem tempore velit. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia nisi doloremque molestiae temporibus illo eos quod sunt vel quo aliquid labore necessitatibus iure ratione, molestias voluptas doloribus minima dicta consequuntur?</p>
-
-    <p class="pt-10 text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora nulla odio ipsum sit asperiores alias iste nesciunt architecto iusto quam, excepturi illo dolor eligendi accusamus, explicabo maxime voluptatem tempore velit. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia nisi doloremque molestiae temporibus illo eos quod sunt vel quo aliquid labore necessitatibus iure ratione, molestias voluptas doloribus minima dicta consequuntur?</p>
-
-    <p class="pt-10 text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora nulla odio ipsum sit asperiores alias iste nesciunt architecto iusto quam, excepturi illo dolor eligendi accusamus, explicabo maxime voluptatem tempore velit. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia nisi doloremque molestiae temporibus illo eos quod sunt vel quo aliquid labore necessitatibus iure ratione, molestias voluptas doloribus minima dicta consequuntur?</p>
+    <p class="pt-10 text-lg sm:text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora nulla odio ipsum sit asperiores alias iste nesciunt architecto iusto quam, excepturi illo dolor eligendi accusamus, explicabo maxime voluptatem tempore velit. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia nisi doloremque molestiae temporibus illo eos quod sunt vel quo aliquid labore necessitatibus iure ratione, molestias voluptas doloribus minima dicta consequuntur?</p>
 </div>
