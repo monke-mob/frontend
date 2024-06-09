873a9aa7c65db395ec71fda8490cfb5a0648a7e7
homie <90294477+homiemace@users.noreply.github.com>
2024-06-09 02:31:25 -0500

    Update aboutUs.svelte

--- src/lib/index/aboutUs.svelte
+++ src/lib/index/aboutUs.svelte
@@ -3,5 +3,5 @@
 <div class="w-screen h-fit py-[15vh] px-6 sm:px-14 lg:px-32" id="#aboutUs">
     <p class="brandTitle">about<br>us</p>
 
-    <p class="pt-10 text-lg sm:text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora nulla odio ipsum sit asperiores alias iste nesciunt architecto iusto quam, excepturi illo dolor eligendi accusamus, explicabo maxime voluptatem tempore velit. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia nisi doloremque molestiae temporibus illo eos quod sunt vel quo aliquid labore necessitatibus iure ratione, molestias voluptas doloribus minima dicta consequuntur?</p>
+    <p class="pt-10 text-lg sm:text-xl">We are small team of game lovers dedicated to making games that'll brighten your day. We're all about crafting unique and fun experiences to put a smile on your face.</p>
 </div>
