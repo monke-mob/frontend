0efdbbb69931e8b9704e6ed8c213d176cd29493e
homie <90294477+homiemace@users.noreply.github.com>
2024-06-09 02:42:35 -0500

    Update aboutUs.svelte

--- src/lib/index/aboutUs.svelte
+++ src/lib/index/aboutUs.svelte
@@ -3,6 +3,6 @@
 <div class="w-screen h-fit py-[15vh] px-6 sm:px-14 lg:px-32" id="#aboutUs">
     <p class="brandTitle">about<br>us</p>
 
-    <p class="pt-10 text-lg sm:text-xl">Welcome to our joyous world! We are a small group of people who love games passionately and are committed to developing games that will brighten your day. Our aim is to design unique and entertaining experiences that make you smile.
+    <p class="pt-10 text-lg sm:text-xl">Welcome! We are a small group of people who love games passionately and are committed to developing games that will brighten your day. Our aim is to design unique and entertaining experiences that make you smile.
 Our journey began in 2021 with the creation of "typical monke game." Initially a simple idea, it has now become our mission to create entertainment and lasting impressions. We believe in the magic of fun strive to put happiness and laughter into every game we make.</p>
 </div>
